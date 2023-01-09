5625689e786bc1810747c696d2ca9ed79966b401
Kuye Harry <dammykuye@gmail.com>
2023-01-09 17:01:08 +0100

    set up models with Prisma

---
+++ source/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import morgan from  'morgan'
+import cors from  'cors'
+import { ErrorHandler } from './middleware/error';
+import { Application } from 'express';
+
+
+
+const app: Application = express();
+   
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+// app.use('/api',router);
+
+app.use("*",ErrorHandler.pagenotFound());
+app.use(ErrorHandler.handle());
+ErrorHandler.exceptionRejectionHandler();
+
+
+export default app
