deb859a58bcc46e737d5c3a67cd79ac19115894f
Kuye Harry <dammykuye@gmail.com>
2023-01-09 20:53:03 +0100

    Input validation for Signup and Signin

--- source/routers/Auth.ts
+++ source/routers/Auth.ts
@@ -2,10 +2,11 @@ import RequestValidator from "../middleware/validation";
 import { protect } from "../middleware/auth";
 import { Router } from "express";
 import  AuthController from "../controllers/Auth";
+import { SignIn, SignUp } from "../../prisma/schemaValidation";
 
 const router = Router();
 
-router.post('/signup', AuthController.signUp)
-router.post('/login', AuthController.signIn)
+router.post('/signup',RequestValidator.validate(SignUp), AuthController.signUp)
+router.post('/login', RequestValidator.validate(SignIn),  AuthController.signIn)
 
 export default router
