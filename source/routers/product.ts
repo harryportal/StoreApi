ced97903c855ba7db2ccb53e351860af2cff14a9
Kuye Harry <dammykuye@gmail.com>
2023-01-11 19:25:21 +0100

    add products controllers

--- source/routers/product.ts
+++ source/routers/product.ts
@@ -6,28 +6,10 @@ import { protect } from '../middleware/auth';
 const productRouter = Router();
 
 
-// returns all products ordered by their date of creation and implement pagination
 productRouter.get('/', Product.getProducts)
-
-// get a single product    
-productRouter.get('/:id', async(req: Request, res: Response)=>{
-
-})
-
-// get products for a particular category order them by thier date of creation and add pagination also
-productRouter.get('/categories/:id', async(req: Request, res: Response)=>{
-
-})
-
-// add rating to a particular product
-productRouter.post('/:id/ratings', protect, async(req: Request, res: Response)=>{
-
-})
-
-// get ratings for a particular product
-productRouter.get('/:id/ratings', async(req: Request, res: Response)=>{
-
-})
+productRouter.get('/:id', Product.getProduct )
+productRouter.get('/categories/:id', Product.getProductsByCategory )
+productRouter.post('/:id/ratings', protect, Product.addProductRating )
 
 export default productRouter
 
