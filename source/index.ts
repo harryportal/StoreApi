8e9e6f602743770cea9a9df2a04af92075b4cfb2
Kuye Harry <dammykuye@gmail.com>
2023-01-17 12:31:29 +0100

    Added Unit Tets

--- source/index.ts
+++ source/index.ts
@@ -3,9 +3,10 @@ import http from 'http'
 import * as dotenv from 'dotenv'
 
 
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 dotenv.config();
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 const startServer = () =>{
     const server: http.Server = app.listen(port, ()=>{
     console.log(`Listening on url http://localhost:${port} `)
