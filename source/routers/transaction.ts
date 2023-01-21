6223bd6399b7e3255af8966defa8042829915148
Kuye Harry <dammykuye@gmail.com>
2023-01-21 23:45:48 +0100

    add transaction routers

---
+++ source/routers/transaction.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { protect } from "../middleware/auth";
+import OrderController from "../controllers/transaction";
+
+
+const transactionRouter = Router()
+
+transactionRouter.post('/create-order', protect, OrderController.createOrder)
+transactionRouter.post('/verify-payment', OrderController.flutterwaveWebhook)
+
+export default transactionRouter
